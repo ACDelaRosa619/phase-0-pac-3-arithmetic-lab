801f4abab1f1dcf20f4a5c2536e67c281ab5d909
Adrian <acdelarosa19@gmail.com>
2023-09-02 09:38:52 -0700

    complete labs

---
+++ index.js
@@ -0,0 +1,15 @@
+function add(a = 540, b = 538) { return a + b}
+
+function subtract(a = 266, b = 700 ) {return a - b}
+
+function multiply(a = 2586, b = 8) {return a * b}
+
+function divide(a = 1.931761786600496, b = 3) {return a / b}
+
+function increment(number) {return ++number}
+
+function decrement(number) {return --number}
+
+function makeInt(n = "40") {return parseInt(n, 10)}
+
+function preserveDecimal(n = "8.322") {return parseFloat(n)}
